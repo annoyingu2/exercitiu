c23f029b8e478b46355b5d647ffedc2a76b95f02
annoyingu2 <remus.joghiu@gmail.com>
2016-06-02 10:07:34 +0300

    An update that includes new error message and style.

--- js/js.js
+++ js/js.js
@@ -1,61 +1,66 @@
 function checkForm(){
-    var errors = '';
+    var formHasErrors = false;
 
     //Reset all errors
-    //var elems = document.querySelectorAll(".error");
     var elems = $(".error");
-    //[].forEach.call(elems, function(el) {
-    //    el.classList.remove("error");
-    //});
-    elems.each(function(index, value){
-        $(value).removeClass("error");
-     });
+    elems.removeClass('error');
+    elems.parent().find(".errorMessage").html("");
 
     var minLength = 3;
     var maxLength = 5;
 
     //Validate names
-    //var names = document.querySelectorAll("#name, #firstname");
     var names = $("#name, #firstname");
     var currentName = null;
     for(var i=0; i<names.length; i++) {
 
         currentName = $(names[i]);
         if (currentName.val() == '')    {
+            formHasErrors = true;
+            currentName.addClass("error");
+            currentName.parent().find(".errorMessage").html("Please fill out the field");
+        } else if (currentName.val().length < minLength || currentName.val().length >maxLength){
+            formHasErrors = true;
+            currentName.addClass("error");
+            currentName.parent().find(".errorMessage").html("Please make sure the input is between 3 - 5 characters long");
+        }
 
-            errors = errors + 'Please fill out the field: ' + currentName.attr("name") + '\n';
-            currentName.addClass("error");
-        }
-        if (currentName.val().length < minLength || currentName.val().length >maxLength){
-            errors = errors + "Please make sure the input is between 3 - 5 characters long" + '\n';
-            currentName.addClass("error");
-        }
     }
 
     //Validate sex
     var sex = $("#sex");
     if(sex.val() != 'F' && sex.val() != 'M') {
-        errors = errors + 'Please fill out the field: ' + sex.attr("name") + '\n';
+        formHasErrors = true;
         sex.addClass("error");
+        sex.parent().find(".errorMessage").html("Please select a gender.");
     }
+
+    //Validate E-mail
+
 
     //Validate pass
     var password =   $("#password");
     var passwordAgain =   $("#password_again");
 
-    if (password.val().length <6){
-        errors = errors + "Please make sure the password is at least 6 characters long." + '\n';
+    if (password.val() == '') {
+        formHasErrors = true;
+        password.parent().find(".errorMessage").html("Please make sure the password is not empty.");
         password.addClass("error");
+    } else if (password.val().length <6){
+        formHasErrors = true;
+        password.addClass("error");
+        password.parent().find(".errorMessage").html("Please make sure the password is at least 6 characters long.");
     }
 
     if (password.val() != passwordAgain.val()){
-        errors = errors + "Please make sure the password are matching." + '\n';
+        formHasErrors = true;
         password.addClass("error");
         passwordAgain.addClass("error");
+        passwordAgain.parent().find(".errorMessage").html("Please make sure the password are matching.");
     }
 
-    if(errors != ''){
-        alert(errors);
+    if(formHasErrors == true){
         return false;
     }
 }
+
